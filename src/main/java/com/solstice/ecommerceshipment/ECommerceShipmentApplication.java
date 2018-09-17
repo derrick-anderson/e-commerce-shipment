64e2ef5ddf13cf4b1c833cf123303b9e712e1d0a
Derrick Anderson <deanderson@solstice.com>
2018-09-17 11:12:59 -0500

    Adds spring cloud Service dependency to all services.

--- src/main/java/com/solstice/ecommerceshipment/ECommerceShipmentApplication.java
+++ src/main/java/com/solstice/ecommerceshipment/ECommerceShipmentApplication.java
@@ -2,8 +2,9 @@ package com.solstice.ecommerceshipment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ECommerceShipmentApplication {
 
     public static void main(String[] args) {
