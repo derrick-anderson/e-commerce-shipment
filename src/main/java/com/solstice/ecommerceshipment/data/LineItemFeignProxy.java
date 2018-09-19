3853acc028b62bc56ff72bf8d356c7ea935def68
Derrick Anderson <deanderson@solstice.com>
2018-09-18 23:19:24 -0500

    Adds fallbacks to Feign clients.

--- src/main/java/com/solstice/ecommerceshipment/data/LineItemFeignProxy.java
+++ src/main/java/com/solstice/ecommerceshipment/data/LineItemFeignProxy.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("e-commerce-order-service")
+@FeignClient(name = "e-commerce-order-service", fallback = LineItemFeignFallback.class)
 @Component
 public interface LineItemFeignProxy {
 
     @RequestMapping(method = RequestMethod.GET, value = "/lines?shipmentId={shipmentId}")
     String getLinesForShipment(@PathVariable("shipmentId") Long shipmentId);
+}
+
+class LineItemFeignFallback implements LineItemFeignProxy{
+
+    @Override
+    public String getLinesForShipment(Long shipmentId){
+        return "{ \"error\" : \"Service Unavailable, please try again later\"}";
+    }
 }
